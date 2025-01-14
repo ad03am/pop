a2548d8b8785a6445de94f04cec275677ca01712
smakuch0 <szymon.d.makuch@gmail.com>
2025-01-14 18:57:46 +0100

    surrogate_model basis

---
+++ src/surrogate_model.py
@@ -0,0 +1,31 @@
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+
+class SurrogateModel:
+    def __init__(self, min_samples = 10):
+        self.model = DecisionTreeRegressor()
+        self.min_samples = min_samples
+        self.history_X = []
+        self.history_y = []
+        
+    def add_sample(self, x, y):
+        self.history_X.append(x)
+        self.history_y.append(y)
+        
+    def train(self):
+        if len(self.history_X) < self.min_samples:
+            return False
+            
+        X = np.array(self.history_X)
+        y = np.array(self.history_y)
+        self.model.fit(X, y)
+        return True
+        
+    def predict(self, X):
+        if len(self.history_X) < self.min_samples:
+            return None
+            
+        if len(X.shape) == 1:
+            X = X.reshape(1, -1)
+            
+        return self.model.predict(X)
